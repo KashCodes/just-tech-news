66e71b004c82c4d034fdba62934bee6406622555
Kashay Arbelo <kashay@agyeman.me>
2021-03-10 06:29:07 -0700

    hide user passwords and connected routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,9 @@ const { User } = require('../../models');
 // GET /api/users
 router.get('/', (req, res) => {
   // Access our User model and run .findAll() method)
-  User.findAll()
+  User.findAll({
+    attributes: { exclude: ['password'] }
+    })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
       console.log(err);
@@ -15,10 +17,11 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
   User.findOne({
+    attributes: { exclude: ['password'] },
     where: {
       id: req.params.id
     }
-  })
+    })
     .then(dbUserData => {
       if (!dbUserData) {
         res.status(404).json({ message: 'No user found with this id' });
