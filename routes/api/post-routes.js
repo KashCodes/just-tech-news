59c2114d6025bf651e7822037f21286bc233e4ae
Kashay Arbelo <kashay@agyeman.me>
2021-03-13 13:37:48 -0700

    refactor upvote model

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -73,36 +73,14 @@ router.post('/', (req, res) => {
     });
 });
 
-// PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-  // create the vote
-  Vote.create({
-    user_id: req.body.user_id,
-    post_id: req.body.post_id
-  }).then(() => {
-    // then find the post we just voted on
-    return Post.findOne({
-      where: {
-        id: req.body.post_id
-      },
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
-        [
-          sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-          'vote_count'
-        ]
-      ]
-    })
-    .then(dbPostData => res.json(dbPostData))
+  // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
     .catch(err => {
       console.log(err);
       res.status(400).json(err);
     });
-  });
 });
 
 router.put('/:id', (req, res) => {
