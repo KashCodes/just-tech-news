c2822a66bc63063b93ed76cd4342eacdfa211fff
Kashay Arbelo <kashay@agyeman.me>
2021-03-13 13:25:03 -0700

    update get routes for posts to include votes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -5,7 +5,13 @@ const { Post, User, Vote } = require('../../models');
 // get all users
 router.get('/', (req, res) => {
   Post.findAll({
-    attributes: ['id', 'post_url', 'title', 'created_at'],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
     order: [['created_at', 'DESC']], 
     include: [
       {
@@ -26,7 +32,13 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id
     },
-    attributes: ['id', 'post_url', 'title', 'created_at'],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
     include: [
       {
         model: User,
