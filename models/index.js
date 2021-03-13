26dcf420d8fe19d11b0d60318c043d002f52d2dc
Kashay Arbelo <kashay@agyeman.me>
2021-03-13 12:06:08 -0700

    votes models added

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require("./Post");
+const Vote = require('./Vote');
 
 // create associations
 User.hasMany(Post, {
@@ -10,5 +11,33 @@ Post.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id'
+});
 
-module.exports = { User, Post };
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
+
+module.exports = { User, Post, Vote };
