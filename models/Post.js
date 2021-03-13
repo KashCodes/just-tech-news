8d88ff5976fe2b23d844c382358951b62ae80dce
Kashay Arbelo <kashay@agyeman.me>
2021-03-13 11:52:42 -0700

    post routes added and tested

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,8 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+
+
 // create our Post model
 class Post extends Model {}
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -35,6 +35,20 @@ router.get('/:id', (req, res) => {
     });
 });
 
+router.post('/', (req, res) => {
+  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 router.post('/login', (req, res) => {
   // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
