12062b4b2c485c95b60102be64c73626abd6a765
Kashay Arbelo <kashay@agyeman.me>
2021-03-13 13:44:04 -0700

    comment model added and connected

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Post = require("./Post");
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
@@ -39,5 +40,21 @@ Post.hasMany(Vote, {
   foreignKey: 'post_id'
 });
 
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+
+module.exports = { User, Post, Vote, Comment };
