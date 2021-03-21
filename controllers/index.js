5f77c19a6f5b9558584fed77c230127c4289c2a4
Kash <kashay@agyeman.me>
2021-03-21 15:42:01 -0700

    edit posts connected and tested

--- controllers/index.js
+++ controllers/index.js
@@ -4,8 +4,8 @@ const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 
-router.use('/dashboard', dashboardRoutes);
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
